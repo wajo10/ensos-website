282c958a224daca07aa8e671f339918cca2d8c87
Wajib Zaglul <wajib10@gmail.com>
2025-09-24 20:28:53 -0600

    [add] Footer desktop with translations

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    TranslatePipe
   ],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
 export class Footer {
 
+  scrollToTop() {
+    if (typeof window !== 'undefined') {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  }
 }
