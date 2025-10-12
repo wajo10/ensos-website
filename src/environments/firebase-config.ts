25ea0a7400dfbc4f5f5d42aa13b315903d0d1b57
Wajib Zaglul <wajib10@gmail.com>
2025-10-11 18:50:59 -0600

    [fix] Syntax

--- src/environments/firebase-config.ts
+++ src/environments/firebase-config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY ?? 'AIzaSyD-LlcQLYYecgafYjIS9XQCEeaWDe_0uIw',
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ?? 'enso-restaurante.firebaseapp.com',
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID ?? 'enso-restaurante',
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ?? 'enso-restaurante.appspot.com',
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID ?? '871441947825',
-  appId: import.meta.env.VITE_FIREBASE_APP_ID ?? '1:871441947825:web:788c7d27d06944dc621690',
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID ?? 'G-2HZMPDPTWT',
+  apiKey:'AIzaSyD-LlcQLYYecgafYjIS9XQCEeaWDe_0uIw',
+  authDomain: 'enso-restaurante.firebaseapp.com',
+  projectId:  'enso-restaurante',
+  storageBucket:  'enso-restaurante.appspot.com',
+  messagingSenderId: '871441947825',
+  appId: '1:871441947825:web:788c7d27d06944dc621690',
+  measurementId: 'G-2HZMPDPTWT',
 } as const;
